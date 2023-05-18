36f0fd48b8e81fb45567501b86457b83b053c3fe
QuocTrieu <21522714@gm.uit.edu.vn>
2023-05-18 23:38:09 +0700

    file

---
+++ src/application/Main.java
@@ -0,0 +1,32 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+			root.requestFocus();
+			Scene scene = new Scene(root,888,618);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			primaryStage.setResizable(false);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,12 @@
+module QLDA {
+	requires javafx.controls;
+	requires javafx.base;
+	requires javafx.fxml;
+	requires javafx.media;
+	requires javafx.graphics;
+	requires com.jfoenix;
+	opens application;
+	opens css;
+	opens img;
+	opens fxml;
+}
